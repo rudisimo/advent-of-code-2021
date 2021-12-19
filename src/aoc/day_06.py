f3c73431d296807fc1e79d5ff331276e5cae5ddf
Rodolfo Puig <rodolfo@puig.io>
2021-12-19 02:24:02 -0500

    Minor refactoring

--- src/aoc/day_06.py
+++ src/aoc/day_06.py
@@ -1,11 +1,13 @@
 from typing import List
 
 
-def evolve(school: List[int], days: int) -> int:
+def evolve(school: List[int], days: int) -> List[int]:
+    # generate list of fish phases
     school_phases = [0] * 9
-    for age in school:
-        school_phases[age] += 1
+    for phase in school:
+        school_phases[phase] += 1
 
+    # evolve fish for n number of days
     for _ in range(days):
         newborn_fish = 0
         for phase in range(9):
@@ -17,18 +19,18 @@ def evolve(school: List[int], days: int) -> int:
         school_phases[8] = newborn_fish
         school_phases[6] += newborn_fish
 
-    return sum(school_phases)
+    return school_phases
 
 
 def solution_1(input: List[str]) -> int:
     normalized_list = [int(d) for d in input.pop(0).split(",")]
-    result = evolve(normalized_list, 80)
+    result = sum(evolve(normalized_list, 80))
 
     return result
 
 
 def solution_2(input: List[str]) -> int:
     normalized_list = [int(d) for d in input.pop(0).split(",")]
-    result = evolve(normalized_list, 256)
+    result = sum(evolve(normalized_list, 256))
 
     return result
